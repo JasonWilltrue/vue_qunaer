315979cc626df0934d44b97d3cd7e54e40165c6a
lovewcc <148373644@qq.com>
2018-12-21 00:36:19 +0800

    项目结构初始化

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
+/*
+ * @Author: Jerrychan
+ * @Date: 2018-12-20 00:11:48
+ * @LastEditors: Jerrychan
+ * @LastEditTime: 2018-12-21 00:32:17
+ * @Description: 入口文件
+ */
+
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router/router';
-import store from './store';
+// import store from './store';
+import fastClick from 'fastclick'; //移动端300ms点击延迟问题解决方案
+import './assets/styles/reset.css';
+import './assets/styles/border.css';
 
 Vue.config.productionTip = false;
+//绑定到根节点上
+fastClick.attach(document.body);
 
 new Vue({
 	router, //router:router  es6写法可以写一个
-	store,
 	render: h => h(App),
 }).$mount('#app');
